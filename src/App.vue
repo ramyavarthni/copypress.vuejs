ac578dc2fb2d082e45f2bf900a360d0e6e1c6827
ramyavarthni <ramyachitra28@gmail.com>
2021-05-17 13:22:34 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/"></router-link> 
+      <router-link to="/about"></router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
